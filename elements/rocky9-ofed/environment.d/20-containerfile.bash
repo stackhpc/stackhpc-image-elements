134c26fbc4141a31ccd46438542ee75dbaeb2316
Stack User <stack@ansiblenode1.cci1.ecmwf.int>
2023-03-30 18:18:01 +0100

    iter

--- elements/rocky9-ofed/environment.d/20-containerfile.bash
+++ elements/rocky9-ofed/environment.d/20-containerfile.bash
@@ -1,3 +1,5 @@
-export DIB_CONTAINERFILE_DOCKERFILE=containerfiles/rocky-9.1-ofed-5.8
-echo "STIG: $(pwd)"
-env
+# note eval to expand the result of the get function
+eval declare -A image_elements=($(get_image_element_array))
+element_path=${image_elements["rocky9-ofed"]}
+
+export DIB_CONTAINERFILE_DOCKERFILE=$element_path/containerfiles/rocky-9.1-ofed-5.8

9bef6a2a5e432e1c8cef8b7f6198eba4af4d5e3d
Stack User <stack@ansiblenode1.cci1.ecmwf.int>
2023-03-30 19:28:19 +0100

    iter

--- elements/rocky9-ofed/environment.d/01-containerfile.bash
+++ elements/rocky9-ofed/environment.d/01-containerfile.bash
@@ -1,5 +1,5 @@
 # note eval to expand the result of the get function
-if [[ -z "$DIB_CONTAINERFILE_DOCKERFILE" ]]
+if [[ -z "${DIB_CONTAINERFILE_DOCKERFILE-}" ]]
 then
     eval declare -A image_elements=($(get_image_element_array))
     element_path=${image_elements["rocky9-ofed"]}

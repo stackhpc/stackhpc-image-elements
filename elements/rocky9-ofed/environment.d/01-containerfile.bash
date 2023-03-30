8f528c920804735199f809f29c0279c173cc90ef
Stack User <stack@ansiblenode1.cci1.ecmwf.int>
2023-03-30 19:24:50 +0100

    iter

---
+++ elements/rocky9-ofed/environment.d/01-containerfile.bash
@@ -0,0 +1,8 @@
+# note eval to expand the result of the get function
+if [[ -z "$DIB_CONTAINERFILE_DOCKERFILE" ]]
+then
+    eval declare -A image_elements=($(get_image_element_array))
+    element_path=${image_elements["rocky9-ofed"]}
+    export DIB_CONTAINERFILE_DOCKERFILE=$element_path/containerfiles/rocky-9.1-ofed-5.8
+fi
+

9e1f305966712d29ed7b20da63de4f8f6b4c1c26
Matt Crees <mattc@stackhpc.com>
2023-06-21 11:05:40 +0100

    Use correct element layout

--- elements/ubuntu-jammy-ofed/environment.d/01-containerfile.bash
+++
@@ -1,7 +0,0 @@
-# note eval to expand the result of the get function
-if [[ -z "${DIB_CONTAINERFILE_DOCKERFILE-}" ]]
-then
-    eval declare -A image_elements=($(get_image_element_array))
-    element_path=${image_elements["ubuntu-jammy-ofed"]}
-    export DIB_CONTAINERFILE_DOCKERFILE=$element_path/containerfiles/ubuntu-jammy-ofed-23.04
-fi
